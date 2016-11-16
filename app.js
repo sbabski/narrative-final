409195f6f45b2481fa3137b9ad008f2f1cb2b3c9
Sarah <babski.s@husky.neu.edu>
2016-11-16 03:25:35 +0000

    save users to db

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const session = require('client-sessions');
 const app = express();
-var db, user;
+var db, currentUser, users;
 
 var anarchy = false;
 var future = false;
@@ -20,7 +20,8 @@ app.use(session({
 app.set('view engine', 'ejs')
 
 MongoClient.connect('mongodb://localhost:27017/', (err, database) => {
-  db = database
+  db = database;
+  users = db.collection('users');
   if (err) return console.log(err)
   console.log('successfully connected to the database');
   app.listen(5000, () => {
@@ -30,7 +31,7 @@ MongoClient.connect('mongodb://localhost:27017/', (err, database) => {
 
 app.get('/', (req, res) => {
   if(req.session && req.session.user) {
-    db.collection('users').findOne({name: req.session.user}, (err, result) => {
+    users.findOne({name: req.session.user}, (err, result) => {
       if(err) return console.log(err)
       user = result
     });
@@ -38,19 +39,31 @@ app.get('/', (req, res) => {
       anarchy: anarchy
     });
   } else {
-    //var users = db.collection('users').find().toArray( (err, results) => {
-      //if (err) return console.log(err)
+    var users = db.collection('users').find().toArray( (err, results) => {
+      if (err) return console.log(err)
       //if new user, create db element
       //else, just load data
-      //console.log(results)
-    //});
+      console.log(results)
+    });
     //return console.log('trying login');
     res.render('pages/login');
   }
 });
 
-app.post('/', (req, res) => {
-  console.log(req.body);
+app.post('/login', (req, res) => {
+  var rb = req.body;
+  rb.future = false;
+  rb.autopsy = false;
+  users.save(rb, (err, result) => {
+    if(err) return console.log(err)
+    console.log('saved to database')
+    res.redirect('/');
+  });
+});
+
+app.get('/dropdb', (req, res) => {
+  db.collection('users').drop();
+  res.redirect('/');
 });
 
 app.get('/autopsy-report', (req, res) => {
