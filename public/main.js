2bc5014ba6e5008ee8c2588246f031484ed2467c
sbabski <babski.s@husky.neu.edu>
2016-11-29 16:38:24 -0500

    add open chat button

--- public/main.js
+++ public/main.js
@@ -1,12 +1,13 @@
-var update = document.getElementById('update')
-
-update.addEventListener('click', function() {
-  fetch('quotes', {
-    method:'put',
-    headers: {'Content-Type': 'application/json'},
-    body: JSON.stringify({
-      'name': 'Darth Vader',
-      'quote': 'I find your lack of faith disturbing.'
-    })
-  })
-});
+function openChat() {
+    setTimeout(function() {
+        var chat = window.open('/chat', 'Chat', 'width=400, height=400');
+        if(chat) {
+            if(chat.closed == false) {
+                chat.close();
+            }
+            window.open('/chat', 'Chat', 'width=400, height=400');
+        } else {
+            console.log('blocked');
+        }
+    }, 3000);
+}
