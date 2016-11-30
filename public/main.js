f8ee6b23c039e70412e0ce08cd9b88e144fa034e
sbabski <babski.s@husky.neu.edu>
2016-11-29 19:17:11 -0500

    testing chat open button

--- public/main.js
+++ public/main.js
@@ -3,16 +3,20 @@ var chat = null;
 function openChat() {
     setTimeout(function() {
         if(!chat) {
-            console.log('testing');
+            chat = window.open('/chat', 'Chat', 'width=400, height=400');
         }
-        var chat = window.open('/chat', 'Chat', 'width=400, height=400');
-        if(chat) {
+        if(!chat) {
+            console.log('blocked');
+        } else {
+            console.log('chat is here');
+        }
+        /*if(chat) {
             if(chat.closed == false) {
                 chat.close();
             }
             window.open('/chat', 'Chat', 'width=400, height=400');
         } else {
             console.log('blocked');
-        }
+        }*/
     }, 3000);
 }
